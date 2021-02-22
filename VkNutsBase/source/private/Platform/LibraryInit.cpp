24e504f9891529930689a767c3f14fa46b69c651
Michael Rizkalla <Michael.Anis1995@gmail.com>
2021-02-22 20:41:50 +0000

    Make libraryInit struct declared in translation unit

--- VkNutsBase/source/private/Platform/LibraryInit.cpp
+++ VkNutsBase/source/private/Platform/LibraryInit.cpp
@@ -5,14 +5,14 @@
 #include <Platform/LibraryInit.h>
 
 namespace nuts {
-    struct LibraryInitOverride final : LibraryInit {
-        LibraryInitOverride()  = default;
-        ~LibraryInitOverride() = default;
+    struct LibraryInit final {
+        LibraryInit() { std::pmr::set_default_resource(get_nuts_memory_resource()); }
+        ~LibraryInit() { std::pmr::set_default_resource(std::pmr::new_delete_resource()); }
     };
 
     namespace {
         static nuts::NutsMemoryResource nuts_memory_resource {};
-        static LibraryInitOverride      nuts_library_init {};
+        static LibraryInit              nuts_library_init {};
     } // namespace
 
     extern "C" NUTS_API std::pmr::memory_resource* get_nuts_memory_resource() noexcept { return std::addressof(nuts_memory_resource); }
